82fd0c4466cf92802067061f6db8872ba757069d
qkrtkdwns3410 <qkrtkdwns3410@github.com>
2022-06-14 21:02:14 +0900

    김영한 스프링 강의

---
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -0,0 +1,17 @@
+package hello.hellospring.service;
+
+/**
+ * packageName    : hello.hellospring.service
+ * fileName       : MemberService
+ * author         : ipeac
+ * date           : 2022-06-14
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2022-06-14        ipeac       최초 생성
+ */
+
+public class MemberService {
+
+}
