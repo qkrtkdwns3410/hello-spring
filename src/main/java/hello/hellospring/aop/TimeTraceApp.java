738c5129b85467b62eee50f97260c709a49d1220
jihye94 <qkrtkdwns3410@naver.com>
2022-06-18 15:26:52 +0900

    spring

---
+++ src/main/java/hello/hellospring/aop/TimeTraceApp.java
@@ -0,0 +1,16 @@
+package hello.hellospring.aop;
+
+/**
+ * packageName    : hello.hellospring.aop
+ * fileName       : TimeTraceApp
+ * author         : jihye94
+ * date           : 2022-06-18
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2022-06-18        jihye94       최초 생성
+ */
+public class TimeTraceApp {
+
+}
